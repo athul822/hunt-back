706133776f388f0f56eb5cbbfcae6e1b621afb83
athul822 <mentalist52@protonmail.com>
2024-09-01 13:19:00 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -3,44 +3,44 @@ const Users = require("../models/users");
 const bcrypt = require("bcrypt");
 
 exports.createUsers = async (req, res) => {
-    try {
-        // Hash the password
-        // const hashPassword = await bcrypt.hash(req.body.password, 10);
-        
-        // Generate unique user ID
-        // req.body.userId = uuidv4();
-        
-        // Store the hashed password
-        // req.body.password = hashPassword;
+  try {
+    // Hash the password
+    // const hashPassword = await bcrypt.hash(req.body.password, 10);
 
-        // Check if user already exists
-        const existingUser = await Users.findOne({ email: req.body.email });
-        if (existingUser) {
-            return res.status(403).json({
-                message: "User with same email already exists. Please sign in",
-            });
-        }
-        // Create the user
-        const newUser = await Users.create(req.body);
-        console.log({ newUser });
+    // Generate unique user ID
+    // req.body.userId = uuidv4();
 
-        // Send success response
-        res.json({ message: "User registration successful", newUser });
-    } catch (error) {
-        // Handle errors
-        console.error("Error in user registration:", error);
-        res.status(500).json({
-            message: "Unable to register new user",
-            error: error.message
-        });
+    // Store the hashed password
+    // req.body.password = hashPassword;
+
+    // Check if user already exists
+    const existingUser = await Users.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(403).json({
+        message: "User with same email already exists. Please sign in",
+      });
     }
+    // Create the user
+    const newUser = await Users.create(req.body);
+    console.log({ newUser });
+
+    // Send success response
+    res.json({ message: "User registration successful", newUser });
+  } catch (error) {
+    // Handle errors
+    console.error("Error in user registration:", error);
+    res.status(500).json({
+      message: "Unable to register new user",
+      error: error.message
+    });
+  }
 };
 
 
 // exports.userLogin = async (req, res) => {
 //     const query = { email: req.body.email };
 //     console.log("user login :", query);
-  
+
 //     Users.findOne(
 //       query
 //     )
@@ -76,27 +76,29 @@ exports.createUsers = async (req, res) => {
 //   };
 
 
-  exports.userLogin = async (req, res) => {
-    const query = { email: req.body.email };
-    console.log("user login :", query);
-  
-    Users.findOne(
-      query
-    )
-      .then((data) => {
-        if (data) {
-          console.log(data, "data");
-          
-        } else {
-          res.status(400).json({
-            message: "No such user",
-          });
-        }
+exports.userLogin = async (req, res) => {
+  const query = { email: req.body.email };
+  console.log("user login :", query);
+
+  Users.findOne(
+    query
+  )
+    .then((data) => {
+      if (data) {
+        return res.status(403).json({
+          message: "sign in success",
+          data: data,
+        });
+      } else {
+        res.status(200).json({
+          message: "No such user",
+        });
+      }
+    })
+    .catch((err) =>
+      res.status(400).json({
+        message: "unable to logins",
+        error: err.message,
       })
-      .catch((err) =>
-        res.status(400).json({
-          message: "unable to logins",
-          error: err.message,
-        })
-      );
-  };
+    );
+};
